a498e611ee7a03396210042d3d4a56cf8d7e5a0d
GiorgioDordo <djole6789@gmail.com>
2024-10-21 15:58:01 +0200

    first step

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+return view('welcome');});
+
+Route::get('/about', function() { return "about benvenuti"; });
