3bf716ea29010b281b5a8a34e4120c5bdb788fd6
GiorgioDordo <djole6789@gmail.com>
2024-10-21 16:09:28 +0200

    created home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>FIRST LARAVEL PROJECT</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
 </head>
 <body>
+    <main>
+        <div class="container mt-5">
+            <h1 class="text-center">
+                <strong>Welcome to laravel</strong>
+            </h1>
+        </div>
+    </main>
 
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-return view('welcome');});
-
-Route::get('/about', function() { return "about benvenuti"; });
+return view('home
+');});
